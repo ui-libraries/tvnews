e1340a9182dd752837cc2bec89d83a2a7a9fbe62
Matthew Butler <matthewtbutler@gmail.com>
2023-05-18 13:30:06 -0500

    switch to YT player module
    it works

--- campaign-youtube-slider/src/index.js
+++ campaign-youtube-slider/src/index.js
@@ -1,6 +1,9 @@
 import * as _ from 'lodash'
 import {videoList} from './videolist'
 const randomVideo = _.sample(videoList)
-let videofragment = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${randomVideo}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`
-document.getElementsByClassName('video-container')[0].innerHTML = videofragment
+let videofragment = `<iframe width="853" height="505" src="https://www.youtube.com/embed/${randomVideo}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`
+document.getElementsByClassName('class-details-container')[0].innerHTML = videofragment
+
+
+
 
