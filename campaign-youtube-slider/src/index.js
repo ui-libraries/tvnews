46944d906e71a9598193f25aaa9ff87dce14ee5a
Matthew Butler <matthewtbutler@gmail.com>
2023-06-13 12:11:52 -0500

    automatic video advance with button

--- campaign-youtube-slider/src/index.js
+++ campaign-youtube-slider/src/index.js
@@ -1,47 +1,50 @@
 import * as _ from 'lodash'
 import YouTubePlayer from 'youtube-player'
-import {videoList} from './videolist'
-const randomVideo = _.sample(videoList)
+import {
+    videoList
+} from './videolist'
+let alreadyPlayed = []
+let randomVideo = _.sample(videoList)
+alreadyPlayed.push(randomVideo)
 let player
 
 player = YouTubePlayer('video-container')
-console.log(player)
 player.loadVideoById(randomVideo)
 let videoValues = []
+let userId = Math.random().toString(36).slice(2)
 
-// add event lister for when input range changes on mouse up
 //player.getVideoData().title
 
 function getLikertLabel(input) {
-    const value = Number(input)  
+    const value = Number(input)
     switch (true) {
-      case value <= 14:
-        return "Strongly Disagree"
-      case value <= 29:
-        return "Disagree"
-      case value <= 39:
-        return "Moderately Disagree"
-      case value <= 49:
-        return "Slightly Disagree"
-      case value <= 59:
-        return "Neither Agree nor Disagree"
-      case value <= 69:
-        return "Slightly Agree"
+        case value <= 14:
+            return "Strongly Disagree"
+        case value <= 29:
+            return "Disagree"
+        case value <= 39:
+            return "Moderately Disagree"
+        case value <= 49:
+            return "Slightly Disagree"
+        case value <= 59:
+            return "Neither Agree nor Disagree"
+        case value <= 69:
+            return "Slightly Agree"
         case value <= 84:
-        return "Moderately Agree"
+            return "Moderately Agree"
         case value <= 90:
-        return "Agree"
+            return "Agree"
         case value <= 100:
-        return "Strongly Agree"
-      default:
-        return "Invalid input"
+            return "Strongly Agree"
+        default:
+            return "Invalid input"
     }
-  }
+}
 
 let slider = document.getElementById('slider')
 let isSliderReleased = false
 
-slider.addEventListener('input', function () {
+slider.addEventListener('input', function() {
     let sliderValue = document.getElementById('slider').value
     sliderValue = _.toNumber(sliderValue)
     document.getElementById('slider-value').innerHTML = sliderValue
@@ -51,27 +54,38 @@ slider.addEventListener('input', function () {
     const blue = 0
     document.getElementById('slidecolor').style.backgroundColor = `rgb(${red}, ${green}, ${blue})`
     isSliderReleased = false
-    slider.addEventListener('change', function () {
+    slider.addEventListener('change', function() {
         if (!isSliderReleased) {
             player.getCurrentTime().then(value => {
                 let newValue = _.toInteger(document.getElementById('slider').value)
                 // create a json object with the current time and the slider value
                 let time = {
-                    currentTime: value,
+                    currentTime: Math.trunc(value),
                     sliderValue: newValue,
                     videoId: randomVideo,
+                    userId: userId
                 }
                 // push the json object to the videoValues array
                 videoValues.push(time)
-                console.log(videoValues)        
+                console.log(videoValues)
             })
             isSliderReleased = true
-        }        
-    })   
+        }
+    })
 })
 
+player.on('stateChange', event => {
+    if (event.data === 0) {
+        // pick a random video from videoList that isn't in alredyPlayed
+        randomVideo = _.sample(_.difference(videoList, alreadyPlayed))
+        alreadyPlayed.push(randomVideo)
+        player.loadVideoById(randomVideo)
+    }
+})
 
-
-
-
-
+// add event listener to the next-video button
+document.getElementById('next-video').addEventListener('click', function() {
+    randomVideo = _.sample(_.difference(videoList, alreadyPlayed))
+    alreadyPlayed.push(randomVideo)
+    player.loadVideoById(randomVideo)
+})
